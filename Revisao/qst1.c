7b501c6f09de55906f0fd158ac82cfaf3c941708
Paulo <dpseutroco@gmail.com>
2022-09-23 23:54:26 +0000

    revisao P1

---
+++ Revisao/qst1.c
@@ -0,0 +1,12 @@
+// Dado o seguinte grafo diga qual a ordem determine a ordem de visita do seguinte grafo:
+// (BFS) - raíz é o 1
+
+// 1-> 2 3
+// 2 -> 1 , 4 , 5
+// 3 -> 5
+// 4 -> 2 5
+// 5 -> 2 , 3
+// 6 -> 2 , 4 , 5
+
+//R: 1 , 2 , 3 , 4, 5
+
---
+++ Revisao/qst2.c
@@ -0,0 +1,13 @@
+// Dado o seguinte grafo descubra a ordem de visita usando BFS e DFS
+
+// 1 -> 2 , 5 , 8
+// 2 -> 1 , 3 , 4
+// 3 -> 2
+// 4 -> 2
+// 5 -> 1 , 7 , 8
+// 6 -> 5  
+// 7 -> 5
+// 8 -> 1 , 9 , 10
+// 9 -> 8
+// 10-> 8
+
